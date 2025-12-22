323f405e0997124c6865b7d508c1e21f924b6ab3
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-12-22 12:55:42 +0530

    Organize Imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
