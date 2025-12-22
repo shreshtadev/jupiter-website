8dfd779a9e6592d748ada6260dbfda3f4768e7bb
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-12-22 12:55:13 +0530

    Enabled Location Frame

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
